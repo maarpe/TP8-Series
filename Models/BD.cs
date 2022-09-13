75b9c967c45867fa3ca2d16f30832c3a9c39234d
MARTIN ARIEL PEREZ <47026770@ort>
2022-09-13 09:03:44 -0300

    Subida del archivo

---
+++ Models/BD.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices.ComTypes;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace TP8_Series.Models
+{
+
+    private static string _connectionString = @"Server=DESKTOP-1RNSEE0\SQLEXPRESS; DataBase=JuegoQQSM;Trusted_Connection=True;";
+
+
+}
+
